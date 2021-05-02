12e15b1cd56a78ccee38ec3bd91895cea11bc27b
pillar <ymblender@gamil.com>
2021-05-02 22:27:07 +0800

    fix: change umd global name

--- rollup.config.js
+++ rollup.config.js
@@ -37,7 +37,7 @@ const watchOptions = [
     output: {
       file: './umd/index.js',
       format: 'umd',
-      name: 'Ob',
+      name: 'For',
       sourcemap: true,
       globals: {
         immer: 'immer'
